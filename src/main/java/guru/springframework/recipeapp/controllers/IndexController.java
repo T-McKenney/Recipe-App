2ea1a5be9e3677c7d938cda0c2b113a1640f74c8
Tyler McKenney <tylermckenney7@gmail.com>
2023-10-04 13:45:23 -0500

    Adds basic index page along with IndexController to handle request mapping.

---
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package guru.springframework.recipeapp.controllers;
+
+/*
+    Created by tylermckenney on 10/4/23.
+*/
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
