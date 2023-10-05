d3b369d21443daf44b74ad862aa889b702fc3f86
Tyler McKenney <tylermckenney7@gmail.com>
2023-10-05 00:10:14 -0500

    Adds Recipe and Notes entities/POJOs. Establishes OneToOne relationship between these entities.

---
+++ src/main/java/guru/springframework/domain/Notes/Notes.java
@@ -0,0 +1,46 @@
+package guru.springframework.domain.Notes;
+
+/*
+    Created by tylermckenney on 10/5/23.
+*/
+
+import guru.springframework.domain.Recipe.Recipe;
+import jakarta.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
