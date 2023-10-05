e3bdbe09eabdc6a284cd1b876e440cb52354c6d7
Tyler McKenney <tylermckenney7@gmail.com>
2023-10-05 12:16:37 -0500

    Adds methods to Category and UOM repositories for accessing description column. Adds constructor to IndexController.

--- src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -4,15 +4,35 @@ package guru.springframework.recipeapp.controllers;
     Created by tylermckenney on 10/4/23.
 */
 
+import guru.springframework.recipeapp.domain.Category;
+import guru.springframework.recipeapp.domain.UnitOfMeasure;
+import guru.springframework.recipeapp.repositories.CategoryRepository;
+import guru.springframework.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
-        System.out.println("Some message to say");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
