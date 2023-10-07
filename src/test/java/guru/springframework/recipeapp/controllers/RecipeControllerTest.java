92a58e730e09d56ad0a27dd466ba291003cd91de
Tyler McKenney <tylermckenney7@gmail.com>
2023-10-07 18:02:06 -0500

    Add in recipe controller for displaying show.html with details about each recipe. Set up unit tests for numerous classes.

---
+++ src/test/java/guru/springframework/recipeapp/controllers/RecipeControllerTest.java
@@ -0,0 +1,54 @@
+package guru.springframework.recipeapp.controllers;
+
+import guru.springframework.recipeapp.domain.Recipe;
+import guru.springframework.recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+/*
+    Created by tylermckenney on 10/7/23.
+*/
+
+class RecipeControllerTest {
+
+    AutoCloseable openMocks;
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController controller;
+
+    @BeforeEach
+    void setUp() {
+
+        openMocks = openMocks(this);
+
+        controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    void testGetRecipe() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}
