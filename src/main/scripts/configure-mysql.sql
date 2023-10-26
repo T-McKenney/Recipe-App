dbd7b2a065d8b28d3fd390168fc673598d00f716
Tyler McKenney <tylermckenney7@gmail.com>
2023-10-26 10:11:06 -0500

    Adds connectivity of Application to external docker MySQL database.
    Utilizes profiles to bootstrap data to targeted databases (default, dev, prod).
    In it's current state, the application will initialize the dev DB with the data-h2.sql information.
    It has functionality through 'BootStrapMySql' to populate the remaining tables, however the data has yet to be binded correctly to the commands.

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -3,26 +3,30 @@ CREATE DATABASE sfg_dev;
 CREATE DATABASE sfg_prod;
 
 # Create DB service accounts
-CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'guru';
-CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'guru';
-CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'guru';
-CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'guru';
+CREATE USER `sfg_dev_user`@'localhost'
+    IDENTIFIED WITH caching_sha2_password BY 'T=6He6X?n?(#Y8$';
+CREATE USER `sfg_dev_user`@'%'
+    IDENTIFIED WITH caching_sha2_password BY 'T=6He6X?n?(#Y8$';
+CREATE USER `sfg_prod_user`@'localhost'
+    IDENTIFIED WITH caching_sha2_password BY 'T=6He6X?n?(#Y8$';
+CREATE USER `sfg_prod_user`@'%'
+    IDENTIFIED WITH caching_sha2_password BY 'T=6He6X?n?(#Y8$';
 
 
 # Database grants
-GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
-GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
-GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
-GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
-GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
-GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
-GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
-GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT SELECT ON sfg_dev.* to `sfg_dev_user`@'localhost';
+GRANT INSERT ON sfg_dev.* to `sfg_dev_user`@'localhost';
+GRANT DELETE ON sfg_dev.* to `sfg_dev_user`@'localhost';
+GRANT UPDATE ON sfg_dev.* to `sfg_dev_user`@'localhost';
+GRANT SELECT ON sfg_prod.* to `sfg_prod_user`@'localhost';
+GRANT INSERT ON sfg_prod.* to `sfg_prod_user`@'localhost';
+GRANT DELETE ON sfg_prod.* to `sfg_prod_user`@'localhost';
+GRANT UPDATE ON sfg_prod.* to `sfg_prod_user`@'localhost';
+GRANT SELECT ON sfg_dev.* to `sfg_dev_user`@'%';
+GRANT INSERT ON sfg_dev.* to `sfg_dev_user`@'%';
+GRANT DELETE ON sfg_dev.* to `sfg_dev_user`@'%';
+GRANT UPDATE ON sfg_dev.* to `sfg_dev_user`@'%';
+GRANT SELECT ON sfg_prod.* to `sfg_prod_user`@'%';
+GRANT INSERT ON sfg_prod.* to `sfg_prod_user`@'%';
+GRANT DELETE ON sfg_prod.* to `sfg_prod_user`@'%';
+GRANT UPDATE ON sfg_prod.* to `sfg_prod_user`@'%';
